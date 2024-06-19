6b24392b32c72d68a86c09bf0f8faf2a3932ae9f
chenxiaoyao6228 <chenxiaoyao6228@163.com>
2024-06-19 12:38:23 +0800

    feat: add icon

--- src/app/editor/features/header/header.component.ts
+++ src/app/editor/features/header/header.component.ts
@@ -1,11 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-editor-header',
   standalone: true,
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  logoPath = 'assets/imgs/header-logo.png';
+  githubLogoPath = 'assets/imgs/github.png';
+}
